13710d71c13eef16994c63f8d122589b2d93ff46
abubaker <mohammad_abubaker@yahoo.com>
2021-11-30 11:25:44 +0500

    Practiced connecting the Fragment and understanding its LifeCycle

--- app/src/main/java/org/abubaker/fragmentsdemo/ExampleFragment.kt
+++ app/src/main/java/org/abubaker/fragmentsdemo/ExampleFragment.kt
@@ -1,6 +1,7 @@
 package org.abubaker.fragmentsdemo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,22 +22,126 @@ class ExampleFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    /**
+     * onCreate()
+     * It take cares of the parameters.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        // Log Entry
+        Log.i("onCreate", "Method is called...")
+
+        // arguments?.let {
+        //    param1 = it.getString(ARG_PARAM1)
+        //    param2 = it.getString(ARG_PARAM2)
+        // }
     }
 
+
+    /**
+     * onCreateView()
+     * Inflates Layout: fragment_example.xml file.
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Log Entry
+        Log.i("onCreateView", "Method was called...")
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_example, container, false)
+
     }
 
+    /**
+     * onViewCreated()
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Log Entry
+        Log.i("onViewCreated", "Method was called...")
+
+    }
+
+    // onViewStateRestored()
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        // Log Entry
+        Log.i("onViewStateRestored", "Method was called...")
+    }
+
+    /**
+     * onStart()
+     */
+    override fun onStart() {
+        super.onStart()
+
+        // Log Entry
+        Log.i("onStart", "Method was called...")
+    }
+
+    /**
+     * onResume()
+     */
+    override fun onResume() {
+        super.onResume()
+
+        // Log Entry
+        Log.i("onResume", "Method was called...")
+    }
+
+    // onPause()
+    override fun onPause() {
+        super.onPause()
+
+        // Log Entry
+        Log.i("onPause", "Method was called...")
+    }
+
+    // onStop()
+    override fun onStop() {
+        super.onStop()
+
+        // Log Entry
+        Log.i("onStop", "Method was called...")
+    }
+
+    // onSaveInstanceState()
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Log Entry
+        Log.i("onSaveInstanceState", "Method was called...")
+    }
+
+    /**
+     * onDestroyView()
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        // Log Entry
+        Log.i("onDestroyView", "Method was called...")
+    }
+
+    /**
+     * onDestroy()
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Log Entry
+        Log.i("onDestroy", "Method was called...")
+    }
+
+    /**
+     * companion object
+     */
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -46,6 +151,7 @@ class ExampleFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ExampleFragment.
          */
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
--- app/src/main/java/org/abubaker/fragmentsdemo/MainActivity.kt
+++ app/src/main/java/org/abubaker/fragmentsdemo/MainActivity.kt
@@ -2,10 +2,28 @@ package org.abubaker.fragmentsdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+
+            supportFragmentManager.commit {
+
+                // Allow Reordering
+                setReorderingAllowed(true)
+
+                // Select the Fragment file to be inflated
+                add<ExampleFragment>(R.id.fragment_container_view)
+
+
+            }
+
+        }
+
     }
 }
